dcb1fc5354ca93e9e68c83f4f95fc8f4efaf0720
Luã Santilli <luasantilli@gmail.com>
2024-03-25 22:12:25 -0300

    please fmt
    Signed-off-by: Luã Santilli <luasantilli@gmail.com>

--- json.go
+++ json.go
@@ -27,8 +27,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-    _, err = w.Write(dat)
-    if err != nil {
-        log.Printf("Error writing JSON: %s", err)
-    }
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Error writing JSON: %s", err)
+	}
 }
