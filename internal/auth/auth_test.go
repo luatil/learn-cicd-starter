09572610900ceb5d48e7e57773a3d6de8ff8ace9
Luã Santilli <luasantilli@gmail.com>
2024-03-25 21:01:33 -0300

    Supposed to fail on ci
    Signed-off-by: Luã Santilli <luasantilli@gmail.com>

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+    "testing"
+    "net/http"
+)
+
+// TestGetAPIKey tests the functionality of GetAPIKey function.
+func TestGetAPIKey(t *testing.T) {
+    expected := "your-expected-api-key"
+    headers := make(http.Header)
+
+    headers.Add("Authorization", "ApiKey " + expected)
+    // Test case 1: No Authorization header included
+    _ , error := GetAPIKey(headers)
+    if error == nil {
+        t.Errorf("Expected no error, got %v", error)
+    }
+}
