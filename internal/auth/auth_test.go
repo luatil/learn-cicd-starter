dfc0cd6f9857564edc1a97b3e40dfe1806594e3d
Luã Santilli <luasantilli@gmail.com>
2024-03-25 21:44:36 -0300

    fmt
    Signed-off-by: Luã Santilli <luasantilli@gmail.com>

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,19 +1,19 @@
 package auth
 
 import (
-    "testing"
-    "net/http"
+	"net/http"
+	"testing"
 )
 
 // TestGetAPIKey tests the functionality of GetAPIKey function.
 func TestGetAPIKey(t *testing.T) {
-    expected := "your-expected-api-key"
-    headers := make(http.Header)
+	expected := "your-expected-api-key"
+	headers := make(http.Header)
 
-    headers.Add("Authorization", "ApiKey " + expected)
-    // Test case 1: No Authorization header included
-    _ , error := GetAPIKey(headers)
-    if error != nil {
-        t.Errorf("Expected no error, got %v", error)
-    }
+	headers.Add("Authorization", "ApiKey "+expected)
+	// Test case 1: No Authorization header included
+	_, error := GetAPIKey(headers)
+	if error != nil {
+		t.Errorf("Expected no error, got %v", error)
+	}
 }
